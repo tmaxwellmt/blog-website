c538bb46ec4994c106c99669d86bb039171d8ab5
tmaxwellmt <tmaxwellmt@gmail.com>
2017-05-11 20:31:50 -0600

    set up containers and components

---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = (props) => {
+  return (
+    <div>
+      <h1>Hello from Home Component</h1>
+    </div>
+  )
+}
+
+export default Home
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router,
+        Route,
+        browserHistory,
+        IndexRoute}
+        from 'react-router'
+
 import App from './App';
+import {HomeContainer} from './containers'
 
 
 ReactDOM.render(
-  <App />,
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <Route path="/Home" component={HomeContainer}/>
+      <IndexRoute component={HomeContainer} />
+    </Route>
+  </Router>,
   document.getElementById('root')
 );
